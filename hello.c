f2e36435ca18bdffc7edcf52776005067b343056
Isaac Osiemo <coderv63@gmail.com>
2016-02-17 15:53:08 +0000

    added comments

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
+/*ithis is header file including prewritten commands  */
 #include  <stdio.h>
-
+/*this is the line that starts the programe*/
 int main(void)
 {
+    /*this line prints a squence of characters*/
     printf("hello, world\n");
 }
