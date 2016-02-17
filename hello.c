bb5bb10ef3435e424a2b4c39f208397efc708b37
Isaac Osiemo <coderv63@gmail.com>
2016-02-17 16:06:48 +0000

    added variables and inputs

--- hello.c
+++ hello.c
@@ -1,8 +1,13 @@
 /*ithis is header file including prewritten commands  */
 #include  <stdio.h>
+#include  <cs50.h>
 /*this is the line that starts the programe*/
 int main(void)
 {
     /*this line prints a squence of characters*/
     printf("hello, world\n");
+    /* using inputs*/
+    string mName = GetString();
+    /*put out the string */
+    printf("%s\n",mName);
 }
