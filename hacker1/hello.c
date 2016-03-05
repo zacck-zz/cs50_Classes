b9dacfd8103f1e87d1fff13d6bb97ba95131c9bb
Isaac Osiemo <coderv63@gmail.com>
2016-03-05 09:04:47 +0000

    Updated hello.c to take in a username had a chance to practice clang with it

--- hacker1/hello.c
+++ hacker1/hello.c
@@ -1,8 +1,15 @@
 #include <stdio.h> 
+#include <cs50.h>
 
 //implement main 
 int main (void)
 {
-    printf("hello, world  from Zacck \n");
+
+    //printf("hello, world  from Zacck \n");
+    printf("Enter Your Name: ");
+    string name = GetString();
+    printf("Hello %s \n", name);
+
 }
+
 
